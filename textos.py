18c21ef949ef0b12b1204b0908d24f2717802da8
John9135 <palacioalexander5@gmail.com>
2023-05-09 12:59:37 -0500

    feat: Se agrego la calse Pergamino y los setter y getter del parametro _antiguedad

--- textos.py
+++ textos.py
@@ -10,7 +10,31 @@ class Textos:
         print(f"El titulo estaba equivocado el nuevo titulo es: {self.titulo}")
 
 
+class Pergamino(Textos):
+    def __init__(self, autor, genero, anio_publicacion, titulo, antiguedad):
+        self._antiguedad = antiguedad
+        super().__init__(autor, genero, anio_publicacion, titulo)
+
+    def cambio_antiguedad(self):
+        print("Escriba la antiguedad del pergamino")
+        antiguedad = input()
+        self.set_antiguedad = antiguedad
+        print(f"La antiguedad de este pergamino es de {self.set_antiguedad}")
+
+    def get_antiguedad(self):
+        return self._antiguedad
+
+    def set_antiguedad(self, antiguedad):
+        self._antiguedad = antiguedad
+
+
 texto1 = Textos("Maria Ferrei", "Fantasia", "2001", "Roma")
 print(texto1.autor, texto1.genero, texto1.anio_publicacion, texto1.titulo)
-
 texto1.cambio_titulo("El nuevo mundo")
+
+pergamino = Pergamino("Desconocido", "Religión", "19 a.c", "Ra", 3000)
+print(pergamino.autor, pergamino.genero,
+      pergamino.anio_publicacion, pergamino.get_antiguedad())
+
+pergamino.cambio_titulo("Historia de RA")
+pergamino.cambio_antiguedad()
