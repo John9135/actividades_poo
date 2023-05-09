25077d64ac1847e0ddcfcbd3de16d60bdc9826ff
John9135 <palacioalexander5@gmail.com>
2023-05-09 10:52:33 -0500

    feat ser creo la clase matematicas y los metodo getter y setter del parametro _nombre_materia

--- materias.py
+++ materias.py
@@ -1,6 +1,6 @@
 class Materias:
     def __init__(self, nombre_materia, grado, nombre_profesor):
-        self.nombre_materia = nombre_materia
+        self._nombre_materia = nombre_materia
         self.grado = grado
         self.nombre_profesor = nombre_profesor
 
@@ -8,8 +8,23 @@ class Materias:
         self.nombre_profesor = nombre_p
         print(f"El nuevo profesor se llama {self.nombre_profesor}")
 
+    def get_nombre_materia(self):
+        return self._nombre_materia
 
-materia = Materias("Matematicas", "8°", "Fernando")
-print(materia.nombre_materia, materia.grado, materia.nombre_profesor)
+    def set_nombre_materia(self, materia):
+        self._nombre_materia = materia
 
+
+class Matematicas(Materias):
+    def __init__(self, nombre_materia, grado, nombre_profesor):
+        super().__init__(nombre_materia, grado, nombre_profesor)
+
+    def nuevo_nombre(self):
+        print(f"El nuevo nombre de la materia es {self.get_nombre_materia()}")
+
+
+materia = Matematicas("Matematicas", "8°", "Fernando")
+print(materia.get_nombre_materia(), materia.grado, materia.nombre_profesor)
 materia.cambio_profesor("Geronimo")
+materia.set_nombre_materia("Matematicas IV")
+materia.nuevo_nombre()
